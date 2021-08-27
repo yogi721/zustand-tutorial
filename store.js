0dedea614ffda2592d493b27ec8d934f0e2e3d24
Mehdi <semlali.mehdi.1980@gmail.com>
2021-08-27 21:17:18 +0200

    adding the store

---
+++ store.js
@@ -0,0 +1,8 @@
+import create from "zustand";
+
+const useStore = create((set) => ({
+    people: ['John Doe', 'Jane Doe'],
+    addPerson: (person) =>
+        set((state) => ({ people: [...state.people, person] }))
+}))
+
